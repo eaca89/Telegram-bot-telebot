fe416c316696ec3f97005047526f70c9c97b2292
ehsan <eaca89@gmail.com>
2024-05-10 11:01:27 -0400

    bot6 updated

--- bot6.py
+++ bot6.py
@@ -18,39 +18,3 @@ def send_update(message):
         bot.send_message(id, "The product is available.")
 
 bot.polling()
-
-
-################ Admin Scenario ########################
-import telebot
-
-# Initialize your bot
-bot = telebot.TeleBot('YOUR_TELEGRAM_BOT_TOKEN')
-
-# Define the admin user ID
-ADMIN_USER_ID = 'admin_user_id_here'
-
-# Command to be executed only by the admin
-@bot.message_handler(commands=['admin_command'])
-def admin_command(message):
-    if str(message.from_user.id) == ADMIN_USER_ID:
-        # Execute admin-only action here
-        bot.reply_to(message, "Admin command executed.")
-    else:
-        bot.reply_to(message, "You are not authorized to execute this command.")
-
-# Handler for broadcasting messages
-@bot.message_handler(func=lambda message: str(message.from_user.id) == ADMIN_USER_ID)
-def broadcast_message(message):
-    # Broadcast message to all subscribers
-    subscribers = []  # Replace with your list of subscriber IDs
-    for subscriber_id in subscribers:
-        bot.send_message(subscriber_id, message.text)
-
-# Start the bot's polling loop
-bot.polling()
-
-
-
-
-
-
