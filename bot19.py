6893a78ceb630641629f274303dfdccd28af0b08
Ehsan <eaca89@gmail.com>
2024-08-20 09:37:14 -0400

    a new feature added to bot19

--- bot19.py
+++ bot19.py
@@ -47,32 +47,56 @@ def handle_group_message(message):
         message_id = message.message_id)
 
 # This function will be called every time a message is sent in the channel
-@bot.channel_post_handler(func=lambda message: True)
-def handle_channel_post(message):
-    channel_id = message.chat.id
+# @bot.channel_post_handler(func=lambda message: True)
+# def handle_channel_post(message):
+#     channel_id = message.chat.id
     # print(f"Channel ID: {channel_id}")
 
 
 # This function forwards every message from channel 1 to channel 2
-@bot.channel_post_handler(content_types=['text', 'audio', 'video', 'document', 'photo', 'sicker', 'voice', 'contact', 'location'])
-def forward_message(message):
-    # channel name test 1
-    channel1 = "-1002204306511"
-    # chennel name test 2
-    channel2 = "-1002246558690"
+# @bot.channel_post_handler(content_types=['text', 'audio', 'video', 'document', 'photo', 'sicker', 'voice', 'contact', 'location'])
+# def forward_message(message):
+#     # channel name test 1
+#     channel1 = "-1002204306511"
+#     # chennel name test 2
+#     channel2 = "-1002246558690"
     
 
-    bot.forward_message(
-        chat_id= channel1,
-        from_chat_id=channel2,
-        message_id = message.message_id,
-        protect_content=True
-    )
+#     bot.forward_message(
+#         chat_id= channel1,
+#         from_chat_id=channel2,
+#         message_id = message.message_id,
+#         protect_content=True
+#     )
 
-    bot.copy_message(
-        chat_id= channel1,
-        from_chat_id=channel2,
-        message_id = message.message_id 
+#     bot.copy_message(
+#         chat_id= channel1,
+#         from_chat_id=channel2,
+#         message_id = message.message_id 
+#     )
+
+# This function forwards every message from channel 1 to group coditize-GP
+# @bot.channel_post_handler(content_types=['text'])
+# def get_chat_id(message):
+#     bot.reply_to(message, f"Chat ID: {message.chat.id}")
+# -1002204306511
+
+# @bot.message_handler(content_types=['text'])
+# def get_chat_id(message):
+#     bot.reply_to(message, f"group id: {message.chat.id}")
+# -4541536669
+
+
+@bot.channel_post_handler(content_types=['text', 'photo', 'video', 'document', 'audio'])
+def forward_post_to_group(message):
+    # Replace with the unique IDs for your private channel and group
+    CHANNEL_ID = -1002204306511  # The private channel's unique ID, must be a negative number
+    GROUP_ID = -4541536669  # The group's unique ID, must be a negative number
+    # Forward the message to the group
+    bot.forward_message(
+        chat_id = GROUP_ID,
+        from_chat_id = CHANNEL_ID,
+        message_id = message.chat.id
     )
 
 bot.polling()
